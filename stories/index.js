88977820bad9effec8f1ca2624d4e9f876185e5f
DESKTOP-TMDJO97\Lee <leethornton1992@hotmail.com>
2019-07-03 22:35:07 +0100

    NewsList component completed

--- stories/index.js
+++ stories/index.js
@@ -3,6 +3,8 @@ import { storiesOf } from '@storybook/react';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import NewsForm from '../src/components/newsForm/';
 import NewsItem from '../src/components/newsItem';
+import NewsList from '../src/components/newsList';
+
 
 const post = {
     id: 1 ,
@@ -13,6 +15,16 @@ const post = {
     upvotes: 10
 };
 
+storiesOf('Hacker App/News List', module)
+    .add('default', () => {
+        const defaultPosts = [
+            { ...post, id: 1, title: 'Post 1', upvotes: 10 },
+            { ...post, id: 2, title: 'Post 2', upvotes: 20 },
+            { ...post, id: 3, title: 'Post 3', upvotes: 30 },
+            { ...post, id: 4, title: 'Post 4', upvotes: 40 }
+        ];
+        return <NewsList posts= { defaultPosts }  />
+    });
 
 storiesOf("Hacker App/News Form", module).add("default", () => <NewsForm />);
 
